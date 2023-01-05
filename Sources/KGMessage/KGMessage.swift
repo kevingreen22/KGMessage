a7a6787304b5cc529a9cda3df8ca35450f72736f
Kevin Green <stuckbykev@gmail.com>
2023-01-04 23:11:43 -0800

    Changes KGMessage from struct to class

--- Sources/KGMessage/KGMessage.swift
+++ Sources/KGMessage/KGMessage.swift
@@ -18,7 +18,7 @@ public struct KGMessageContent {
     }
 }
                                    
-public struct KGMessage {
+public class KGMessage {
     
     public init() {}
     
@@ -26,7 +26,7 @@ public struct KGMessage {
     ///
     /// - Paremeter content: The type of messageContent to apply to the email message.
     /// - Parameter addresses: An array of string containing the email addresses to sent the message to.
-    public static func sendEmail(content: KGMessageContent, addresses: [String]) -> some View {
+    public class func sendEmail(content: KGMessageContent, addresses: [String]) -> some View {
         return KGComposeMailView(result: .constant(nil), toRecipients: addresses, message: content, isHTML: false)
     }
     
@@ -36,7 +36,7 @@ public struct KGMessage {
     ///
     /// - Parameter content: The type of MessageContent to apply to the SMS message.
     /// - Parameter phoneNumber: The phone number to send the SMS messge to.
-    public static func sendSMSText(content: KGMessageContent, phoneNumbers: [String]) -> KGComposeSMS {
+    public class func sendSMSText(content: KGMessageContent, phoneNumbers: [String]) -> KGComposeSMS {
         let cleanPhoneNumbers = phoneNumbers.map { $0.components(separatedBy: CharacterSet.decimalDigits.inverted).joined() }
         return KGComposeSMS(smsResult: .constant(nil), recipients: cleanPhoneNumbers, messageContent: content, attachements: nil)
     }
@@ -46,7 +46,7 @@ public struct KGMessage {
     /// Starts a phone call.
     ///
     /// - Parameter phoneURL: Concatenates the phone number to call after the "telpromp:' prefix as a URL.
-    public static func callphoneNumber(phoneNumber: String) {
+    public class func callphoneNumber(phoneNumber: String) {
         let cleanPhoneNumber = phoneNumber.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
         if let phoneURL = URL(string: "telprompt://\(cleanPhoneNumber)") /*, UIApplication.shared.canOpenURL(callPhoneURL)*/ {
             if #available(iOS 10, *) {
